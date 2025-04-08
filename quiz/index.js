6644dbc4eece17117d80550ff5ef2f0e07f8e6e4
Rich Couzzi <rich.couzzi@kernel.inc>
2025-04-08 14:45:49 -0400

    adding quiz demo, removing test suite python in favor of private repo

---
+++ quiz/index.js
@@ -0,0 +1,186 @@
+
+const style = 'background-color: darkblue; color: white; font-style: italic; padding:.2em; font-size: 30px;'
+document.addEventListener("DOMContentLoaded", () => {
+ 
+  function drawQuiz(
+    answerTextArray,
+    choicesTextArray,
+    pointsArray,
+    profilesJSON
+  ) {
+
+// ------------------------------------------------------
+// @DRAWQUIZ VARIABLES ---------------------------------
+// ------------------------------------------------------
+     
+    let answerText = choicesTextArray;
+    let answerValues = [];
+    let buttonElement = document.getElementById("button");
+    let quiz = document.getElementById("quiz");
+    let questionState = 0;
+    let quizActive = true;
+    let questionText = answerTextArray;
+    let results = document.getElementById("results");
+    let totalPoints = 0;
+
+// ------------------------------------------------------
+// @DRAWQUIZ METHODS -----------------------------------
+// ------------------------------------------------------
+    function updateQuizState() {
+     
+      updateProfile();
+
+      if (quizActive) {
+        drawQuestion(questionState);
+        questionState++;
+        buttonElement.disabled = true;
+        buttonElement.innerHTML = "Please select an answer";
+        buttonElement.style.opacity = 0.7;
+      } else {
+        drawProfilePage();
+      }
+    }
+
+    function drawQuestion(question) {
+      let answerSelection = ``;
+
+      for (i = 0; i < answerText[question].length; i++) {
+        let answerChoice = answerText[question][i]
+        answerSelection += `
+          <li> 
+            <input type="radio" name="question${question + 1}" 
+                   onClick="window.setAnswer(${i},${question})" 
+                   id="${answerChoice}">
+
+              <label for="${answerChoice}">
+                ${answerChoice}
+              </label>
+          </li>`;
+      }
+
+      document.getElementById("questions").innerHTML = questionText[question];
+      document.getElementById("answers").innerHTML = answerSelection;
+    }
+
+    function setAnswer(input, questionIndex) {
+      answerValues[questionIndex] = pointsArray[questionIndex][input];
+      let letter = String.fromCharCode(65 + input);
+      console.log(
+        `%c#${questionIndex + 1} | ${letter}  +${
+          pointsArray[questionIndex][input]
+        }`,style
+      );
+
+      if (questionState < questionText.length) {
+        buttonElement.innerHTML = "Continue";
+        buttonElement.disabled = false;
+        buttonElement.style.opacity = 1;
+      } else {
+        quizActive = false;
+        buttonElement.innerHTML = "Crunch Numbers";
+        buttonElement.disabled = false;
+        buttonElement.style.opacity = 1;
+      }
+    }
+
+    function updateProfile() {
+      totalPoints = answerValues.reduce(
+        (total, current) => total + Number(current),
+        0
+      );
+     
+
+      // console.log(`\r-----------------\r`);
+      console.clear()
+      console.log(`%cTotal Points: ${totalPoints}`, style);
+      document.title = `Quiz | ${answerValues.length}/${questionText.length} | Points: ${totalPoints}`;
+    }
+
+    function drawProfilePage() {
+      quiz.style.display = "none";
+      results.style.display = "block";
+      let resultsInfo;
+      profilesJSON.forEach((profile) => {
+        let lowerBound = parseInt(profile["Lower Bound"]);
+        let upperBound = parseInt(profile["Upper Bound"]);
+        if (totalPoints >= lowerBound && totalPoints <= upperBound) {
+          resultsInfo = profile;
+        }
+        // console.log(
+        //   totalPoints,
+        //   totalPoints >= lowerBound && totalPoints <= upperBound
+        // );
+      });
+      results.innerHTML = `
+        <!-- <h1>${totalPoints}</h1> -->
+        <h2>${resultsInfo["You Are:"]}</h2>
+        <h3>${resultsInfo["Sandwich"]}</h3>
+
+        <p>${resultsInfo["Description"]}</p>
+        <button class="button" onClick="javascript:window.location.href=window.location.href">Restart quiz</button>
+        <!-- <pre>${JSON.stringify(resultsInfo, null, 2)}</pre> -->
+    `;
+    }
+
+    buttonElement.addEventListener("click", updateQuizState);
+    window.setAnswer = setAnswer;
+  }
+
+// ------------------------------------------------------
+// @SHEETDB CONFIG --------------------------------------
+// ------------------------------------------------------
+  let sheetDB = {
+    enpoint: `https://sheetdb.io/api/v1/c1ckdgz5om2wt`
+  }
+
+  sheetDB = {
+    ...sheetDB,
+    info: {
+      live: `${sheetDB.enpoint}/name`,
+      cached: "cached/cached.info.json",
+    },
+    quiz: {
+      live: `${sheetDB.enpoint}?sheet=Quiz%20Questions%20and%20Feedback`,
+      cached: "cached/cached.quiz.json",
+    },
+    scoring: {
+      live: `${sheetDB.enpoint}?sheet=Scoring Matrix`,
+      cached: `cached/cached.scoring.json`,
+    },
+    profiles: {
+      live: `${sheetDB.enpoint}?sheet=Results Profiles`,
+      cached: "cached/cached.profiles.json",
+    },
+  };
+
+  fetchAll(sheetDB.quiz.cached, sheetDB.scoring.cached, sheetDB.profiles.cached)
+    .then((results) => {
+      let quizJSON = results.response1;
+      let scoreJSON = results.response2;
+      let profilesJSON = results.response3;
+      let answerTextArray = [];
+      let choicesTextArray = [];
+      let pointsArray = [];
+
+      Object.keys(quizJSON).forEach((json, i) => {
+        answerTextArray.push(quizJSON[json]["Question"]);
+        choicesTextArray.push([
+          quizJSON[json]["A"],
+          quizJSON[json]["B"],
+          quizJSON[json]["C"],
+          quizJSON[json]["D"],
+        ]);
+        pointsArray.push([
+          scoreJSON[i]["A"],
+          scoreJSON[i]["B"],
+          scoreJSON[i]["C"],
+          scoreJSON[i]["D"],
+        ]);
+      });
+
+      drawQuiz(answerTextArray, choicesTextArray, pointsArray, profilesJSON);
+    })
+    .catch((error) => {
+      console.error("Failed to fetch data:", error);
+    });
+});
